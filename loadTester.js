f59a719cdefe31ed955e5ee3b7fe7de362cea66e
Ja-on John Kim <jaonjohnkim@github.com>
2017-11-03 13:07:43 -0700

    minor change to loadTester

--- loadTester.js
+++ loadTester.js
@@ -4,6 +4,6 @@ setInterval(() => {
   // const zipcodes = [94102, 94111];
   // let zipcode = zipcodes[Math.round(Math.random())];
   zipcode = 94111;
-  request(`https://fireincident.herokuapp.com/json?zipcode=${zipcode}&startDate=2017-07-01T00:00:00.000&endDate=2017-10-25T00:00:00.000&granularity=month`)
+  request(`http://34.238.93.174:8080/json?zipcode=${zipcode}&startDate=2017-07-01T00:00:00.000&endDate=2017-10-25T00:00:00.000&granularity=month`)
   console.log('Pinged for zipcode:', zipcode);
-}, 50);
+}, 100);
