51f6f2f745148eb299c320585f39ddda295b160d
Ja-on John Kim <jaonjohnkim@github.com>
2017-11-04 12:05:40 -0700

    req obj

--- server/index.js
+++ server/index.js
@@ -19,21 +19,26 @@ const app = express();
 app.get('/*', async (req, res) => {
   statsDClient.increment('.service.fire.query.all');
   const start = Date.now();
-
+  let {zipcode, startDate, endDate, granularity} = req.query;
 
   const today = db.stringifyDate(new Date());
   let threeMonthsAgo = new Date();
   threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 3);
   const threeMonthsAgoStr = db.stringifyDate(threeMonthsAgo);
 
-  req.zipcode = req.zipcode || 94102;
-  req.startDate = req.startDate || threeMonthsAgoStr;
-  req.endDate = req.endDate || today;
-  req.granularity = req.granularity || 'week';
-  let {zipcode, startDate, endDate, granularity} = req.query;
+  zipcode = zipcode || 94102;
+  startDate = startDate || threeMonthsAgoStr;
+  endDate = endDate || today;
+  granularity = granularity || 'week';
+  const reqObj = {
+    zipcode: zipcode,
+    startDate: startDate,
+    endDate: endDate,
+    granularity: granularity
+  };
   // console.log("startDate:", startDate, typeof startDate);
   // console.log("endDate:", endDate, typeof endDate);
-  const reply = await redis.getFromCache(req.query)
+  const reply = await redis.getFromCache(reqObj);
   if (reply) {
     res.status(200).send(JSON.parse(reply));
     statsDClient.timing('.service.fire.query.latency_ms', Date.now() - start);
@@ -51,7 +56,7 @@ app.get('/*', async (req, res) => {
         statsDClient.increment('.service.fire.query.db');
         if (data) {
           // console.log('About to cache', req.query);
-          redis.addToCache(req.query, data, null);
+          redis.addToCache(reqObj, data, null);
         }
       } else {
         res.status(400).send('Outside of boundary');
